cba87517a02e1d1bcd023d2127b96fd28ab4dbf3
Glenn <C23454056@mytudublin.ie>
2025-10-24 12:19:47 +0100

    Docker compose and redis caching for flask app - lab 6

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,21 @@
-FROM python:3.10
+FROM python:3.10-slim
 
 WORKDIR /app
 
+# Copy dependency list
+COPY requirements.txt .
+
+# Install all Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy all project files into the image
 COPY . .
 
-RUN pip install Flask
-
+# Flask environment variables
 ENV FLASK_APP=hello.py
 ENV FLASK_RUN_HOST=0.0.0.0
 
 EXPOSE 5000
 
-CMD ["flask" , "run"]
+# Command to start Flask
+CMD ["flask", "run"]
--- hello.py
+++ hello.py
@@ -1,29 +1,68 @@
 from flask import Flask
+import redis, time
 
 app = Flask(__name__)
+
+r = redis.Redis(host='redis-server',  port=6379, decode_responses=True);
 
 @app.route("/")
 def home():
 
-	return '''
-		<p>Hello, World, I am a flask App</p>
+	data = r.get('home');
+
+	if data is not None:
+
+		if time.time() - float(r.get('home_time')) < 600:
+
+			return data
 		
-		
-		'''
+	data = '<p>Hello World, i am a flask app</p>'
+
+	r.set('home',data)
+	r.set('home_time',time.time())
+
+	return data
 
 @app.route("/about")
 def about():
 
-	return '''
+	data = r.get('about')
+
+	if data is not None:
+
+		if time.time() - float(r.get('about_time')) < 600:
+
+			return data
+		
+	data =  '''
 		<p>This app is running on the flask web framework</p>
 		<p>learn more about flask here</p>
 		<a href="https://flask.palletsprojects.com/">Flask framework Documentation</a></p>
 	
 		'''
+	
+	r.set('about',data)
+	r.set('about_time', time.time())
+
+	return data
 
 @app.route("/contact")
 def contact():
-	return "<p>Contact Me At: c23454056@mytudublin.ie</p>"
+
+	data = r.get('contact')
+
+	if data is not None:
+
+		if time.time() - float(r.get('contact_time')) < 600:
+
+			return data
+
+	data = "<p>Contact Me At: c23454056@mytudublin.ie</p>"
+
+	r.set('contact',data)
+	r.set('contact_time',time.time())
+
+	return data
 
 
 if __name__ == "__main__":
